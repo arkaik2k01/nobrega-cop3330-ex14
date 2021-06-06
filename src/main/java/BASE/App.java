4cfdb98444d25c30d2ebfc48a084bd202dd9d6f8
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2021-06-05 21:44:55 -0400

    I almost made 50 if statements, one for each state.... Done with base case. Done with challenges

--- src/main/java/BASE/App.java
+++ src/main/java/BASE/App.java
@@ -1,6 +1,72 @@
 package BASE;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Miguel Nobrega
+ */
+
+/*
+Exercise 14 - Tax Calculator
+You don’t always need a complex control structure to solve simple problems. Sometimes a program requires an extra step in one case, but in all other cases there’s nothing to do.
+
+Write a simple program to compute the tax on an order amount. The program should prompt for the order amount and the state.
+If the state is “WI,” then the order must be charged 5.5% tax. The program should display the subtotal, tax, and total for Wisconsin residents
+but display just the total for non-residents.
+
+Example Output
+What is the order amount? 10
+What is the state? WI
+The subtotal is $10.00.
+The tax is $0.55.
+The total is $10.55.
+Or
+What is the order amount? 10
+What is the state? MN
+The total is $10.00
+
+Constraints
+Implement this program using only a simple if statement—don’t use an else clause.
+Ensure that all money is rounded up to the nearest cent.
+Use a single output statement at the end of the program to display the program results.
+
+Challenges
+Allow the user to enter a state abbreviation in upper, lower, or mixed case.
+Also allow the user to enter the state’s full name in upper, lower, or mixed case.
+ */
+
+import java.util.Scanner;
+
 public class App
 {
+    static Scanner in = new Scanner(System.in);
+    static final double TAX = 0.055;
 
+    public static void main(String[] args)
+    {
+        App prog = new App();
+
+        //input
+        System.out.print("What is the order amount? ");
+        double order = in.nextDouble();
+        System.out.print("What is the state? ");
+        String state = in.next();
+
+        //Calculation, check and output
+        prog.sendOutput(state, order);
+    }
+
+    public double calcTax(double order)
+    {
+        return (double)order * TAX;
+    }
+
+    public void sendOutput(String state, double order)
+    {
+        if(state.equalsIgnoreCase("Wi") || state.equalsIgnoreCase("Wisconsin"))
+        {
+            System.out.printf("The subtotal is $%,.2f%nThe tax is $%,.2f%nThe total is %,.2f", order, calcTax(order), (calcTax(order) + order));
+            return;
+        }
+        System.out.printf("The total is $%,.2f", order);
+    }
 }
